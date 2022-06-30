99e42e38185978bffe3e93fc2a645fec56f1f88d
Rachel Brasileiro <rachel.lopes.bras@gmail.com>
2022-06-29 21:36:43 -0300

    teste

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,6 +3,7 @@ import { LogoProps } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
+
 const Logo = ({ alternativeText, url }: LogoProps) => (
   <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
